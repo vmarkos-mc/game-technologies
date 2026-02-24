9cb380c7f00bf2aaf0b3b52c013688069675a715
vmarkos-mc <v.markos@mc-class.gr>
2026-02-24 09:55:07 +0200

    W04 1h

---
+++ Week 04 - Revision Lab(s)/in-class/Game_Lab_01/Game_Lab_01/GameGrid.cs
@@ -0,0 +1,104 @@
+﻿using System;
+
+namespace Game_Lab_01
+{
+	public class GameGrid
+	{
+		// By default, each game grid has two dimensions.
+		internal int rows; // We need those to be visible to child classes
+        internal int cols;
+		private Player player; // This declares an object of the class Player
+
+		// General purpose grid constructor.
+		public GameGrid(int rows, int cols)
+		{
+			this.rows = rows;
+			this.cols = cols;
+			player = new Player(this.rows - 1, 0);
+		}
+
+		// Just a shorthand to create square game grids.
+		public GameGrid(int rows) : this(rows, rows) { }
+
+        // Get neighbours
+        public virtual GridPoint GetLeft(GridPoint p)
+        {
+            // If we are already at the leftmost part, then we just return ourselves.
+            if (p.GetY() == 0) return p;
+            // If we are not at the lefmost part of the grid, then just return a point that is one column left.
+            return new GridPoint(p.GetX(), p.GetY() - 1);
+        }
+
+        public virtual GridPoint GetRight(GridPoint p)
+        {
+            if (p.GetY() == cols - 1) return p;
+            return new GridPoint(p.GetX(), p.GetY() + 1);
+        }
+
+        public virtual GridPoint GetUp(GridPoint p)
+		{
+			if (p.GetX() == 0) return p;
+			return new GridPoint(p.GetX() - 1, p.GetY());
+		}
+
+        public virtual GridPoint GetDown(GridPoint p)
+		{
+			if (p.GetX() == rows - 1) return p;
+			return new GridPoint(p.GetX() + 1, p.GetY());
+		}
+
+        // Move related utilities
+
+        // Makes a move, if allowed, or throw an exception
+        public void MakeMove(MoveDirection direction)
+        {
+            // Get player's current location
+            GridPoint playerLocation = player.GetLocation();
+            GridPoint newPlayerLocation; // Just an object declaration
+            switch (direction)
+            {
+                case MoveDirection.Left: // if
+                    newPlayerLocation = GetLeft(playerLocation);
+                    break;
+                case MoveDirection.Right: // else if
+                    newPlayerLocation = GetRight(playerLocation);
+                    break;
+                case MoveDirection.Up: // else if
+                    newPlayerLocation = GetUp(playerLocation);
+                    break;
+                case MoveDirection.Down: // else if
+                    newPlayerLocation = GetDown(playerLocation);
+                    break;
+                default: // else
+                    throw new ArgumentOutOfRangeException("Invalid move option."); // In case we have been provided with an irrelevant value.
+            }
+            player.SetLocation(newPlayerLocation);
+        }
+
+        // Local definition of possible move directions
+        public enum MoveDirection
+        {
+            Left, Right, Up, Down // 0, 1, 2, 3, respectively
+        }
+
+        public override string ToString()
+        {
+			string gridString = ""; // or string gridString = string.Empty;
+			for (int i = 0; i < rows; i++)
+			{
+				for (int j = 0; j < cols; j++)
+				{
+					if (player.IsAt(i, j))
+					{
+						gridString += $"{player} ";
+					} else
+					{
+                        gridString += "* "; // You might want to *not* print the empty character for j == cols - 1
+                    }
+				}
+				gridString += "\n";
+			}
+			return gridString;
+        }
+	}
+}
---
+++ Week 04 - Revision Lab(s)/in-class/Game_Lab_01/Game_Lab_01/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+
+namespace Game_Lab_01
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int size = GetGridSize();
+            bool wrap = GetWrap();
+            GameGrid grid = wrap ? new WrappingGameGrid(size) : new GameGrid(size);
+            Console.WriteLine(grid);
+            while (true)
+            {
+                GameGrid.MoveDirection direction = ParseMoveString();
+                grid.MakeMove(direction);
+                /*
+                 * Ideas to produce a grid that does not print over and over again:
+                 *  1. Just print many (many) newlines.
+                 *      - Major issue: window rescaling.
+                 *      - Another major issue: which screen is the biggest one?
+                 *  2. Move cursor up by some lines and then print over the existing grid again.
+                 *  3. Move cursor to current player position, erase player,
+                 *      move cursor to new player position, print player.
+                 */
+                Console.WriteLine("\x1b[{0}A{1}", size + 2, grid);
+            }
+        }
+
+        public static bool GetWrap()
+        {
+            Console.Write("Wrap or no wrap (y/n): ");
+            return Console.ReadLine().ToLower() == "y"; // So, anything else other than "y" returns false;
+        }
+
+        public static GameGrid.MoveDirection ParseMoveString()
+        {
+            // This assignment is just a hack to stop the compiler complaining.
+            GameGrid.MoveDirection direction = GameGrid.MoveDirection.Down;
+            bool validDirection = false;
+            while (!validDirection)
+            {
+                Console.Write("Next move (wasd): ");
+                string moveString = Console.ReadLine();
+                try
+                {
+                    direction = GetMoveDirection(moveString);
+                    validDirection = true;
+                }
+                catch (ArgumentOutOfRangeException e)
+                {
+                    // Just ignore it
+                }
+            }
+            return direction;
+        }
+
+        public static GameGrid.MoveDirection GetMoveDirection(string moveString)
+        {
+            switch (moveString)
+            {
+                case "w":
+                    return GameGrid.MoveDirection.Up; // No need for break here, since we return.
+                case "a":
+                    return GameGrid.MoveDirection.Left;
+                case "s":
+                    return GameGrid.MoveDirection.Down;
+                case "d":
+                    return GameGrid.MoveDirection.Right;
+                default:
+                    throw new ArgumentOutOfRangeException("Invalid move option. Valid ones: 'w', 'a', 's', 'd'.");
+            }
+        }
+
+        /*
+         * We can think of enums as named integers, i.e., integers that also
+         * have a semantic name, as in our case, for grid sizes.
+         */
+        public enum GridSize
+        {
+            Small = 15,
+            Medium = 20,
+            Large = 30
+        }
+
+        public static int GetGridSize()
+        {
+            Console.Write("Please enter grid size (Small=15, Medium=20, Large=30): ");
+            int size = Int32.Parse(Console.ReadLine());
+            while (!GridSize.IsDefined(typeof(GridSize), size))
+            {
+                Console.Write("Please enter grid size (Small=15, Medium=20, Large=30): ");
+                size = Int32.Parse(Console.ReadLine());
+            }
+            return size;
+        }
+    }
+}
