fa63335673ae3012e1eb48d03a2ffbf77300d89e
bill <vassileiosmarkos@gmail.com>
2026-03-02 20:22:31 +0200

    Add W05

---
+++ Week 05 - Code Refactoring (Lab)/in-class/Game_Lab_01/Game_Lab_01/GameGrid.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Game_Lab_01
+{
+	public class GameGrid
+	{
+		// By default, each game grid has two dimensions.
+		internal int rows; // We need those to be visible to child classes
+        internal int cols;
+		private Player player; // This declares an object of the class Player
+        // HashSet is like Python's set.
+        private HashSet<GridPoint> visited = new HashSet<GridPoint>();
+        private HashSet<GridPoint> relics = new HashSet<GridPoint>();
+
+		// General purpose grid constructor.
+		public GameGrid(int rows, int cols)
+		{
+			this.rows = rows;
+			this.cols = cols;
+			player = new Player(this.rows - 1, 0);
+            relics = GetRandomGridPoints(rows);
+		}
+           
+        // Just a shorthand to create square game grids.
+		public GameGrid(int rows) : this(rows, rows) { }
+
+        // Get neighbours
+        public virtual GridPoint GetLeft(GridPoint p)
+        {
+            // If we are already at the leftmost part, then we just return ourselves.
+            if (p.GetY() == 0) return p;
+            // If we are not at the lefmost part of the grid, then just return a point that is one column left.
+            return new GridPoint(p.GetX(), p.GetY() - 1);
+        }
+
+        public virtual GridPoint GetRight(GridPoint p)
+        {
+            if (p.GetY() == cols - 1) return p;
+            return new GridPoint(p.GetX(), p.GetY() + 1);
+        }
+
+        public virtual GridPoint GetUp(GridPoint p)
+		{
+			if (p.GetX() == 0) return p;
+			return new GridPoint(p.GetX() - 1, p.GetY());
+		}
+
+        public virtual GridPoint GetDown(GridPoint p)
+		{
+			if (p.GetX() == rows - 1) return p;
+			return new GridPoint(p.GetX() + 1, p.GetY());
+		}
+
+
+        private HashSet<GridPoint> GetRandomGridPoints(int N)
+        {
+            HashSet<GridPoint> points = new HashSet<GridPoint>();
+            // This is not uniformly random, so we should use something like reservoir sampling here.
+            while (points.Count < N)
+            {
+                GridPoint point = GetRandomGridPoint();
+                points.Add(point);
+            }
+            return points;
+        }
+
+        private GridPoint GetRandomGridPoint()
+        {
+            Random rng = new Random(); // This is a (pseudo)random number generator
+            int x = rng.Next(0, rows); // Random.Next() returns the next random number.
+            int y = rng.Next(0, cols);
+            GridPoint point = new GridPoint(x, y);
+            return point;
+        }
+
+        // Move related utilities
+
+        // Makes a move, if allowed, or throw an exception
+        public void MakeMove(MoveDirection direction)
+        {
+            // Get player's current location
+            GridPoint playerLocation = player.GetLocation();
+            // `Add()` adds an element to a set only if it is not already there
+            visited.Add(playerLocation);
+            GridPoint newPlayerLocation; // Just an object declaration
+            switch (direction)
+            {
+                case MoveDirection.Left: // if
+                    newPlayerLocation = GetLeft(playerLocation);
+                    break;
+                case MoveDirection.Right: // else if
+                    newPlayerLocation = GetRight(playerLocation);
+                    break;
+                case MoveDirection.Up: // else if
+                    newPlayerLocation = GetUp(playerLocation);
+                    break;
+                case MoveDirection.Down: // else if
+                    newPlayerLocation = GetDown(playerLocation);
+                    break;
+                default: // else
+                    throw new ArgumentOutOfRangeException("Invalid move option."); // In case we have been provided with an irrelevant value.
+            }
+            player.SetLocation(newPlayerLocation);
+        }
+
+        // Local definition of possible move directions
+        public enum MoveDirection
+        {
+            Left, Right, Up, Down // 0, 1, 2, 3, respectively
+        }
+
+        public override string ToString()
+        {
+			string gridString = ""; // or string gridString = string.Empty;
+			for (int i = 0; i < rows; i++)
+			{
+				for (int j = 0; j < cols; j++)
+				{
+					if (player.IsAt(i, j))
+					{
+						gridString += $"{player} ";
+					} else if (visited.Contains(new GridPoint(i, j)))
+                    {
+                        gridString += ". "; // This means we have visited this cell
+                    } else if (relics.Contains(new GridPoint(i, j)))
+                    {
+                        gridString += "+ "; // Just for debugging purposes
+                    }
+                    else
+                    {
+                        gridString += "* "; // You might want to *not* print the empty character for j == cols - 1
+                    }
+				}
+                if (i < rows - 1) gridString += "\n";
+			}
+			return gridString;
+        }
+	}
+}
---
+++ Week 05 - Code Refactoring (Lab)/in-class/Game_Lab_01/Game_Lab_01/GridPoint.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Game_Lab_01
+{
+    // Since we need this class only within our namespace, we define this as `internal`
+    public class GridPoint
+    {
+        private int x;
+        private int y;
+
+        public GridPoint(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public int GetX()
+        {
+            return x;
+        }
+
+        public int GetY()
+        {
+            return y;
+        }
+
+        // No setters is just like x and y are constants.
+        public string ToString()
+        {
+            return String.Format("({0}, {1})", x, y);
+        }
+
+        public override bool Equals(object? other)
+        {
+            // This, in practice, means that two `GridPoint`s compare equal exactly
+            // when they have the same coordinates and not when they are in the same place
+            // in memory.
+            if (other == null) return false;
+            if (other.GetType() != typeof(GridPoint)) return false;
+            GridPoint otherPoint = (GridPoint) other;// Alternative: `as GridPoint`;
+            return this.x == otherPoint.x && this.y == otherPoint.y; // `this` is redundant here, just emphasis
+        }
+
+        /*
+         * When we override `Equals()` we typically have to override `GetHashCode()` in a way that:
+         *  1. When `Equals()` returns `true`, `GetHashCode()` should return the same hash value.
+         *  2. When `Equals()` returns `false`, *optionally*, `GetHashCode()` returns different hash values.
+         */
+        public override int GetHashCode()
+        {
+            return x.GetHashCode() ^ y.GetHashCode();
+        }
+
+        /*
+         * In general, in OOP, besides primitives, everything is passed by *reference*, i.e., 
+         * we pass as an argument a **reference** to the memory location where the object 
+         * is kept.
+         * 
+         * This also means that, when compaing obejcts, e.g., when we check whether an element
+         * is contained in a set or not, we use its *reference* and not it actual field value(s).
+         * 
+         * We can override this behaviour by overriding the Equals function.
+         */
+    }
+}
