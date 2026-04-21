5bc08055c278415010d84ae20c322d2cf8309354
bill <vassileiosmarkos@gmail.com>
2026-04-21 09:58:28 +0300

    Add W10 materials

---
+++ Week 10 - 2D Level Design/in-class/Game_Lab_01/Game_Lab_01/GameUI.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Game_Lab_01
+{
+    public class GameUI
+    {
+        // More on the singleton pattern:
+        // https://refactoring.guru/design-patterns/singleton
+        private Game game;
+        private bool wrap;
+        private int size;
+        // We implement the Singleton Design Pattern, so we
+        // declare a private reference to ourselves.
+        private static GameUI uiInstance;
+
+        private GameUI()
+        {
+            // Empty for now, so just reserving memory
+        }
+
+        // This is the actual singleton constructor
+        public static GameUI Instance()
+        {
+            // If there is no instance initialised, create one
+            if (uiInstance == null) { uiInstance = new GameUI(); }
+            // Then, return the private UI instance.
+            return uiInstance;
+        }
+
+        public void InitialiseGame()
+        {
+            GetGridSize();
+            GetWrap();
+            game = new Game(size, wrap);
+        }
+
+        public void Run()
+        {
+            Console.WriteLine(game);
+            while (!game.IsOver())
+            {
+                GameGrid.MoveDirection direction = ParseMoveString();
+                game.MakeMove(direction);
+                Console.WriteLine("\x1b[{0}A\r{1}", size, game);
+                // FIXME: Maybe improve alignment
+                KeyValuePair<string, int> sanitisedInventory = SanitisedInventory();
+                int verticalInventoryOffset = size - sanitisedInventory.Value;
+                Console.Write("\x1b[{0}A\x1b[{1}C{2}\x1b[{3}B", size, 2 * size, sanitisedInventory.Key, verticalInventoryOffset);
+            }
+            Console.WriteLine("You won in {0} moves!", game.GetNumberOfMoves());
+            while (Console.ReadKey().KeyChar != 'q') { Console.Write("\rPress 'q' to exit!"); }
+        }
+
+        public void GetWrap()
+        {
+            Console.Write("Wrap or no wrap (y/n): ");
+            wrap = Console.ReadLine().ToLower() == "y"; // So, anything else other than "y" returns false;
+        }
+
+        /*
+         * We can think of enums as named integers, i.e., integers that also
+         * have a semantic name, as in our case, for grid sizes.
+         */
+        private enum GridSize
+        {
+            Small = 15,
+            Medium = 20,
+            Large = 30
+        }
+
+        public void GetGridSize()
+        {
+            Console.Write("Please enter grid size (Small=15, Medium=20, Large=30): ");
+            size = Int32.Parse(Console.ReadLine());
+            while (!GridSize.IsDefined(typeof(GridSize), size))
+            {
+                Console.Write("Please enter grid size (Small=15, Medium=20, Large=30): ");
+                size = Int32.Parse(Console.ReadLine());
+            }
+        }
+
+        public GameGrid.MoveDirection ParseMoveString()
+        {
+            // This assignment is just a hack to stop the compiler complaining.
+            GameGrid.MoveDirection direction = GameGrid.MoveDirection.Down;
+            bool validDirection = false;
+            while (!validDirection)
+            {
+                //Console.Write("Next move (wasd): ");
+                // string moveString = Console.ReadLine();
+                //char moveString = (char) Console.Read(); // Works but leads to some UI bugs
+                // ConsoleKeyInfo stores information about the last key pressed by user.
+                // key.KeyChar corresponds to the character of that key (if it does exist).
+                ConsoleKeyInfo key = Console.ReadKey();
+                char moveChar = key.KeyChar;
+                try
+                {
+                    direction = GetMoveDirection(moveChar);
+                    validDirection = true;
+                }
+                catch (ArgumentOutOfRangeException)
+                {
+                    // Just ignore it
+                }
+            }
+            return direction;
+        }
+
+        private GameGrid.MoveDirection GetMoveDirection(char moveString)
+        {
+            switch (moveString)
+            {
+                case 'w':
+                    return GameGrid.MoveDirection.Up; // No need for break here, since we return.
+                case 'a':
+                    return GameGrid.MoveDirection.Left;
+                case 's':
+                    return GameGrid.MoveDirection.Down;
+                case 'd':
+                    return GameGrid.MoveDirection.Right;
+                default:
+                    throw new ArgumentOutOfRangeException("Invalid move option. Valid ones: 'w', 'a', 's', 'd'.");
+            }
+        }
+
+        private KeyValuePair<string, int> SanitisedInventory()
+        {
+            string inventoryString = game.GetPlayerInventory().ToString();
+            int lines = inventoryString.Count(c => c == '\n');
+            string saveCursorPosition = "\x1b[s";
+            string moveCursorSequence = "\x1b[u\x1b[1B\x1b[s";
+            string sanitisedInventoryString = saveCursorPosition + inventoryString.Replace("\n", moveCursorSequence);
+            return new KeyValuePair<string, int>(sanitisedInventoryString, lines);
+        }
+    }
+}
---
+++ Week 10 - 2D Level Design/in-class/Game_Lab_01/Game_Lab_01/Knife.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Game_Lab_01
+{
+    internal class Knife : Equipment, ICollects
+    {
+        private HashSet<Ingredient.IngredientType> collectibles;
+
+        public Knife(GridPoint location) : base(location)
+        {
+            collectibles = new HashSet<Ingredient.IngredientType>() {
+                Ingredient.IngredientType.Spanaki,
+                Ingredient.IngredientType.Vrouva, 
+                Ingredient.IngredientType.Prasso,
+                Ingredient.IngredientType.Anithos,
+                Ingredient.IngredientType.Seskoulo,
+            };
+        }
+
+        public bool CanCollect(Ingredient ingredient)
+        {
+            return collectibles.Contains(ingredient.GetIngredientType());
+        }
+
+        public override string ToString()
+        {
+            return "Knife"; // Dummy as it gets, but, it works...
+        }
+    }
+}
