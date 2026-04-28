955d1f6c9bd40a9c5dfe2fba90fff29cb6732aac
vmarkos-mc <v.markos@mc-class.gr>
2026-04-28 09:46:28 +0300

    Add W11 tutorial

--- Week 09 - CBT/in-class/unity-introduction/unity-introduction/Assets/Scripts/PlayerController.cs
+++ Week 09 - CBT/in-class/unity-introduction/unity-introduction/Assets/Scripts/PlayerController.cs
@@ -5,18 +5,20 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    private readonly float STEP = 0.1f;
+    private readonly float STEP = 3.0f;
     public InputAction MoveAction;
 
     void Start()
     {
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = 10;
         MoveAction.Enable();
     }
 
     void Update()
     {
         Vector2 move = MoveAction.ReadValue<Vector2>();
-        Vector2 position = (Vector2) transform.position + move * STEP;
+        Vector2 position = (Vector2) transform.position + move * STEP * Time.deltaTime;
         transform.position = position;
     }
 }
