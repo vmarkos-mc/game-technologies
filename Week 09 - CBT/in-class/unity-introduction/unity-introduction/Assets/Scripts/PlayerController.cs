174a2d732a8af1d9c90a6d3bfe2f67c282dcb11f
vmarkos-mc <v.markos@mc-class.gr>
2026-04-28 12:13:58 +0300

    Add W11 in-class

--- Week 09 - CBT/in-class/unity-introduction/unity-introduction/Assets/Scripts/PlayerController.cs
+++ Week 09 - CBT/in-class/unity-introduction/unity-introduction/Assets/Scripts/PlayerController.cs
@@ -7,18 +7,27 @@ public class PlayerController : MonoBehaviour
 {
     private readonly float STEP = 3.0f;
     public InputAction MoveAction;
+    Rigidbody2D rigidBody2d;
+    Vector2 move;
 
     void Start()
     {
-        QualitySettings.vSyncCount = 0;
-        Application.targetFrameRate = 10;
+        //QualitySettings.vSyncCount = 0;
+        //Application.targetFrameRate = 10;
+        rigidBody2d = GetComponent<Rigidbody2D>();
         MoveAction.Enable();
     }
 
     void Update()
     {
-        Vector2 move = MoveAction.ReadValue<Vector2>();
-        Vector2 position = (Vector2) transform.position + move * STEP * Time.deltaTime;
-        transform.position = position;
+        move = MoveAction.ReadValue<Vector2>();
+        //Vector2 position = (Vector2) transform.position + move * STEP * Time.deltaTime;
+        //transform.position = position;
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 position = (Vector2)rigidBody2d.position + move * STEP * Time.deltaTime;
+        rigidBody2d.MovePosition(position);
     }
 }
